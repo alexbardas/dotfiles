f2fe7cf40a2a382727d415905920715ea2a63f72
Alex Bardas <alexbardas@gmail.com>
2015-05-26 21:28:41 +0100

    Updates + monitor time spent running every command

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,5 @@
 mkdir -p ~/.vim/backup ~/.vim/tmp
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+mkdir -p ~/z && git clone https://github.com/rupa/z.git ~/z \
+    && cp ~/z/z.sh ~/.bash_z && rm -rf ~/z
